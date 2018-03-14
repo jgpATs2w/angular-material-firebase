1e6634baae6fd1692426a7c044127535976ec2b9
Javier Garcia <jgp@sens2web.es>
2018-03-14 07:04:39 +0100

    added firebase to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBANcbhuGQQ1Kednh2aj2Wm9VANnNPYxPo",
+    authDomain: "test-c6e9c.firebaseapp.com",
+    databaseURL: "https://test-c6e9c.firebaseio.com",
+    projectId: "test-c6e9c",
+    storageBucket: "test-c6e9c.appspot.com",
+    messagingSenderId: "203861923976"
+  }
 };
